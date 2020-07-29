9e0458900d65522c1e452683027c7976f6342d68
Utkarsh Lath <utkarshlath2@gmail.com>
2020-07-29 23:24:07 +0530

    Initial Commit

---
+++ Life, the Universe, and Everything.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    cin>>number;
+    while(number!=42)
+    {
+        cout<<number<<endl;
+        cin>>number;
+    }
+    return 0;
+}

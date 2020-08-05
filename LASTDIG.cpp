98d374d6c42157913aebe1e985eea7aa95a53aa4
Utkarsh Lath <utkarshlath2@gmail.com>
2020-08-05 23:30:34 +0530

    Binary Exponentiation

---
+++ LASTDIG.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+long long binpow(long long a, long long b, long long m) {
+    a %= m;
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int a,b;
+        cin>>a>>b;
+        long long result = binpow(a,b,10);
+        cout<<result<<endl;
+    }
+    return 0;
+}

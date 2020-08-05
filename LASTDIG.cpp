d13da6bd08d7713ecf83789dc9ce48d5e9583653
Utkarsh Lath <utkarshlath2@gmail.com>
2020-08-06 00:15:54 +0530

    Binary Exponentiation

--- LASTDIG.cpp
+++ LASTDIG.cpp
@@ -5,10 +5,10 @@ long long binpow(long long a, long long b, long long m) {
     a %= m;
     long long res = 1;
     while (b > 0) {
-        if (b & 1)
-            res = res * a % m;
-        a = a * a % m;
-        b >>= 1;
+        if ((b%2)==1)  // equivalent to (b&1)
+            res = (res * a) % m;
+        a = (a * a) % m;
+        b=b/2;         // equivalent to b >>= 1;
     }
     return res;
 }

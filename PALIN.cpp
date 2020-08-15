ab2ef2bf0512b6e733f088bcb0ff12b000305717
Utkarsh Lath <utkarshlath2@gmail.com>
2020-08-15 21:30:50 +0530

    Better way for Palindrome

--- PALIN.cpp
+++ PALIN.cpp
@@ -1,37 +1,54 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-string findSum(string str1, string str2) 
-{ 
-    string str = ""; 
-    if (str1.length() > str2.length()) 
-        swap(str1, str2); 
-    int n1 = str1.length(), n2 = str2.length(); 
+string allnine(string s, int len)
+{
+    string result(1, '1');
+    for(int i=1;i<=len-1;i++)
+        result+='0';
+    result += '1';
+    return result;
+}
 
-    reverse(str1.begin(), str1.end()); 
-    reverse(str2.begin(), str2.end()); 
-  
-    int carry = 0; 
-    for (int i=0; i<n1; i++) 
-    { 
-        int sum = ((str1[i]-'0')+(str2[i]-'0')+carry); 
-        str.push_back(sum%10 + '0'); 
-        carry = sum/10; 
-    } 
-  
-    for (int i=n1; i<n2; i++) 
-    { 
-        int sum = ((str2[i]-'0')+carry); 
-        str.push_back(sum%10 + '0'); 
-        carry = sum/10; 
-    } 
-  
-    if (carry) 
-        str.push_back(carry+'0'); 
- 
-    reverse(str.begin(), str.end()); 
-  
-    return str; 
+string findPalindrome(string s, int len)
+{
+    string left,right,result;
+    left = s.substr(0,len/2);
+    if(len%2!=0)
+        right = s.substr(len/2+1,len/2);
+    else
+        right = s.substr(len/2,len/2);
+    int sublen = left.length();
+    for(int i=sublen-1,j=0;i>=0,j<sublen;i--,j++)
+    {
+        if((left[i]-'0')==(right[j]-'0'))
+            continue;
+        if((left[i]-'0')>(right[j]-'0'))
+        {
+            result=left;
+            string rev(result.rbegin(),result.rend());
+            if(len%2!=0)
+                result+= s[len/2];
+            result += rev;
+            return result;
+        }
+        if((left[i]-'0')<(right[j]-'0'))
+            break;
+    }
+    int carry=0;
+    for(int i=ceil(float(len)/2)-1;i>=0;i--)
+    {
+        int x = s[i]-'0';
+        if(i==(ceil(float(len)/2)-1))
+            x=x+1;
+        if(carry)
+            x = x+carry;
+        carry = x/10;
+        x=x%10;
+        s[i]=x+'0';
+        s[len-i-1]=x+'0';
+    }
+    return s;
 }
 
 int main()
@@ -41,13 +58,21 @@ int main()
     string one(1,'1');
     while(t--)
     {
-        string str;
+        string str,result;
         cin>>str;
         str.erase(0, min(str.find_first_not_of('0'), str.size()-1));
-        str=findSum(str,one);
-        while((string(str.rbegin(),str.rend()))!=str)
-            str=findSum(str,one);
-        cout<<str<<endl;
+        int flag=0,len=str.length();
+        for(int i=0;i<len;i++)
+            if(str[i]!='9')
+                flag=1;
+        if(flag==0)
+        {
+            result = allnine(str,len);
+            cout<<result<<endl;
+            continue;
+        }
+        result = findPalindrome(str,len);
+        cout<<result<<endl;
     }
     return 0;
 }

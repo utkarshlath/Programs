d885c92d0b122f7f335547a896e0b640202b0bd9
Utkarsh Lath <utkarshlath2@gmail.com>
2020-08-29 22:32:16 +0530

    Try segment tree

--- GSS1.cpp
+++ GSS1.cpp
@@ -1,22 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int  solveQuery(int arr[], int n, int x, int y)
+void construct_tree(int arr[], int seg[], int low, int high, int pos)
 {
-    int max = INT_MIN, max_x=INT_MIN;
-    for(int i=x-1;i<=y-1;i++)
+    if(low==high)
     {
-        max_x = arr[i];int sum = arr[i];
-        for(int j=i+1;j<=y-1;j++)
-        {
-            sum+= arr[j];
-            if(sum>max_x)
-                max_x = sum;
-        }
-        if(max_x>max)
-            max=max_x;
+        seg[pos]=arr[low];
+        return;
     }
-    return max;
+    int mid = (low+high)/2;
+    construct_tree(arr,seg,low,mid,2*pos+1);
+    construct_tree(arr, seg, mid+1,high,2*pos+2);
+    seg[pos] = max(seg[2*pos+1],max(seg[2*pos+2], seg[2*pos+1]+seg[2*pos+2]));
+}
+
+int solveQuery(int seg[], int qlow, int qhigh, int low, int high, int pos)
+{
+    if(qlow<=low&&high<=qhigh)
+        return seg[pos];
+    if(qlow>high||low>qhigh)
+        return INT_MIN;
+    int mid = (low+high)/2;
+    return max(solveQuery(seg,qlow,qhigh,low,mid,2*pos+1),solveQuery(seg,qlow,qhigh,mid+1,high,2*pos+2));
 }
 
 int main()
@@ -26,13 +31,22 @@ int main()
     int arr[n];
     for(int i=0;i<n;i++)
         cin>>arr[i];
+    int seglen;
+    if((ceil(log2(n))==floor(log2(n))))
+        seglen = n*2-1;
+    else
+        seglen = pow(2,(ceil(log2(n))))*2-1;
+    int seg[seglen];
+    for(int i=0;i<seglen;i++)
+        seg[i]=INT_MIN;
+    construct_tree(arr,seg,0,n-1,0);
     int m;
     cin>>m;
     while(m--)
     {
         int x,y;
         cin>>x>>y;
-        cout<<solveQuery(arr,n,x,y)<<endl;
+        cout<<solveQuery(seg,x-1,y-1,0,n-1,0)<<endl;
     }
     return 0;
 }

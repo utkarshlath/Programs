3c91d2eee0dff9872fda0e5d85980dbb43c28743
Utkarsh Lath <utkarshlath2@gmail.com>
2020-08-30 11:45:34 +0530

    Segment Tree

--- GSS1.cpp
+++ GSS1.cpp
@@ -1,27 +1,75 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define inf 0x3f3f 
 
-void construct_tree(int arr[], int seg[], int low, int high, int pos)
+struct Node { 
+    int maxPrefixSum; 
+    int maxSuffixSum; 
+    int totalSum; 
+    int maxSubarraySum; 
+   
+    Node() 
+    { 
+        maxPrefixSum = maxSuffixSum = maxSubarraySum = -inf; 
+        totalSum = -inf; 
+    } 
+};
+Node merge(Node left, Node right)
+{
+    Node parent;
+    parent.maxPrefixSum = max(left.maxPrefixSum,left.totalSum+right.maxPrefixSum);
+    parent.maxSuffixSum = max(right.maxSuffixSum, right.totalSum+left.maxSuffixSum);
+    parent.totalSum = left.totalSum+right.totalSum;
+    parent.maxSubarraySum = max({left.maxSubarraySum,right.maxSubarraySum,left.maxSuffixSum+right.maxPrefixSum});
+    return parent;
+}
+void construct_tree_util(Node*tree, int arr[], int low, int high, int pos)
 {
     if(low==high)
     {
-        seg[pos]=arr[low];
+        tree[pos].maxPrefixSum=arr[low];
+        tree[pos].maxSuffixSum=arr[low];
+        tree[pos].totalSum=arr[low];
+        tree[pos].maxSubarraySum=arr[low];
         return;
     }
     int mid = (low+high)/2;
-    construct_tree(arr,seg,low,mid,2*pos+1);
-    construct_tree(arr, seg, mid+1,high,2*pos+2);
-    seg[pos] = max(seg[2*pos+1],max(seg[2*pos+2], seg[2*pos+1]+seg[2*pos+2]));
+    construct_tree_util(tree,arr,low,mid,2*pos);
+    construct_tree_util(tree,arr, mid+1,high,2*pos+1);
+    tree[pos] = merge(tree[2*pos],tree[2*pos+1]);
+}
+Node* construct_tree(int arr[],int n)
+{
+    
+    int seglen;
+    if((ceil(log2(n))==floor(log2(n))))
+        seglen = n*2-1;
+    else
+        seglen = pow(2,(ceil(log2(n))))*2-1;
+    Node* tree = new Node[seglen];
+    construct_tree_util(tree,arr,0,n-1,1);
+    return tree;
 }
 
-int solveQuery(int seg[], int qlow, int qhigh, int low, int high, int pos)
+Node solveQueryUtil(Node*tree, int low, int high, int qlow, int qhigh, int pos)
 {
-    if(qlow<=low&&high<=qhigh)
-        return seg[pos];
-    if(qlow>high||low>qhigh)
-        return INT_MIN;
-    int mid = (low+high)/2;
-    return max(solveQuery(seg,qlow,qhigh,low,mid,2*pos+1),solveQuery(seg,qlow,qhigh,mid+1,high,2*pos+2));
+    if(high<qlow||qhigh<low)
+    {
+        Node nullnode;
+        return nullnode;
+    }
+    if(low>=qlow&&qhigh>=high)
+        return tree[pos];
+    int mid = (low + high)/2; 
+    Node left = solveQueryUtil(tree, low, mid, qlow, qhigh, 2*pos); 
+    Node right = solveQueryUtil(tree, mid + 1, high, qlow, qhigh, 2*pos + 1);  
+    Node res = merge(left, right); 
+    return res; 
+}
+int solveQuery(Node* tree, int low, int high, int n)
+{
+    Node res = solveQueryUtil(tree, 0, n - 1, low, high, 1); 
+    return res.maxSubarraySum; 
 }
 
 int main()
@@ -31,22 +79,14 @@ int main()
     int arr[n];
     for(int i=0;i<n;i++)
         cin>>arr[i];
-    int seglen;
-    if((ceil(log2(n))==floor(log2(n))))
-        seglen = n*2-1;
-    else
-        seglen = pow(2,(ceil(log2(n))))*2-1;
-    int seg[seglen];
-    for(int i=0;i<seglen;i++)
-        seg[i]=INT_MIN;
-    construct_tree(arr,seg,0,n-1,0);
+    Node* tree = construct_tree(arr,n);
     int m;
     cin>>m;
     while(m--)
     {
         int x,y;
         cin>>x>>y;
-        cout<<solveQuery(seg,x-1,y-1,0,n-1,0)<<endl;
+        cout<<solveQuery(tree,x-1,y-1,n)<<endl;
     }
     return 0;
 }

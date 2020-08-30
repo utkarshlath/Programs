f6ecafd414dda368b7eb3d1dbba0352e71e9c138
Utkarsh Lath <utkarshlath2@gmail.com>
2020-08-30 23:46:23 +0530

    Topdown approach

--- FARIDA.cpp
+++ FARIDA.cpp
@@ -1,12 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+long long int memo[10001];
+
 long long int findResult(long long int arr[], long long int n)
 {
     if(n<0)
         return 0;
-    return max(findResult(arr,n-2)+arr[n],findResult(arr,n-1));
+    if(memo[n]!=-1)
+        return memo[n];
+    return memo[n]= max(findResult(arr,n-2)+arr[n],findResult(arr,n-1));
 }
+
 int main()
 {
     long long int t,caseno=0;
@@ -14,11 +19,14 @@ int main()
     while(t--)
     {
         caseno++;
+        memset(memo,-1,sizeof(memo));
         long long int n;
         cin>>n;
         long long int arr[n];
         for(long long int i=0;i<n;i++)
             cin>>arr[i];
+        memo[0]=arr[0];
+        memo[1]=max(arr[0],arr[1]);
         cout<<"Case "<<caseno<<": "<<findResult(arr,n-1)<<endl;
     }
     return 0;

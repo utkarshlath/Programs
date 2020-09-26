f6253359b912e6d08fdbff234c0a570dc8b658bb
Utkarsh Lath <utkarshlath2@gmail.com>
2020-09-26 23:33:28 +0530

    Remove

--- HOTELS.cpp
+++ HOTELS.cpp
@@ -10,16 +10,7 @@ int main()
     vector<ll> v(n);
     for(ll i=0;i<n;i++)
         cin>>v[i];
-    int max_so_far = INT_MIN, max_ending_here = 0; 
-    for (int i = 0; i < n; i++) 
-    { 
-        max_ending_here = max_ending_here + v[i]; 
-        if (max_so_far < max_ending_here && max_ending_here <= m) 
-            max_so_far = max_ending_here; 
-  
-        if (max_ending_here < 0) 
-            max_ending_here = 0; 
-    } 
+    
     cout<<total_max;
     return 0;
 }

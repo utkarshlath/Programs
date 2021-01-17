046e4407212e8c209a4fcc60404d436b91411e8d
Utkarsh Lath <utkarshlath2@gmail.com>
2021-01-17 18:16:26 +0530

    Moore's Voting Algorithm

---
+++ InterviewBit/Arrays/N3RepeatNumber.cpp
@@ -0,0 +1,42 @@
+int Solution::repeatedNumber(const vector<int> &A) {
+    int count1 = 0, count2 = 0;
+    int first=INT_MAX, second=INT_MAX    ;
+    int n = A.size();
+    for(int i = 0; i < n; i++) {
+ 
+        if(first == A[i])
+            count1++;
+        else if(second == A[i])
+            count2++;
+        else if(count1 == 0) {
+            count1++;
+            first = A[i];
+        }
+        else if(count2 == 0) {
+            count2++;
+            second = A[i];
+        }
+        else {
+            count1--;
+            count2--;
+        }
+    }
+ 
+    count1 = 0;
+    count2 = 0;
+ 
+    for(int i = 0; i < n; i++) {
+        if (A[i] == first)
+            count1++;
+        else if (A[i] == second)
+            count2++;
+    }
+
+    if(count1 > n / 3)
+        return first;
+ 
+    if(count2 > n / 3)
+        return second;
+ 
+    return -1;
+}

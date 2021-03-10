95dc0a3822e2a5ad2f09451e0db1ad4e162aeed7
Utkarsh Lath <utkarshlath2@gmail.com>
2021-03-10 23:13:32 +0530

    Add File

---
+++ InterviewBit/Binary Search/AddBinaryStrings.cpp
@@ -0,0 +1,38 @@
+int binaryAdd(int a, int b){
+    if(a==0&&b==0)
+        return 0;
+    if(a==1&&b==1)
+        return 0;
+    return 1;
+}
+string Solution::addBinary(string A, string B) {
+    string Arev = A;
+    reverse(Arev.begin(),Arev.end());
+    string Brev = B;
+    reverse(Brev.begin(),Brev.end());
+    string ans="";
+    int carry=0;
+    while(Arev.size() != Brev.size()){
+        if(Arev.size() < Brev.size())
+            Arev.push_back('0');
+        else if(Arev.size() > Brev.size())
+            Brev.push_back('0');
+    }
+    int len = Arev.length();
+    if(len==0) 
+        return Arev;
+    for(int i=0;i<len;i++){
+        int a = Arev[i]-'0';
+        int b = Brev[i]-'0';
+        int val = a+b+carry;
+        ans+=((val%2)+'0');
+        carry=(val)/2;
+    }
+    
+    if(carry != 0) 
+        ans.push_back(carry+'0');
+    while(ans.size() > 0 && ans[ans.size()-1] == '0') 
+        ans.pop_back();
+    reverse(ans.begin(),ans.end());
+    return ans;
+}

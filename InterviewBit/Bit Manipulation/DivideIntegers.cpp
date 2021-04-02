f31541e924bc6a2222b81a51459ac32118a70463
Utkarsh Lath <utkarshlath2@gmail.com>
2021-04-02 14:02:45 +0530

    Add File

---
+++ InterviewBit/Bit Manipulation/DivideIntegers.cpp
@@ -0,0 +1,14 @@
+int Solution::divide(int A, int B) {
+    if(A==INT_MIN&&B==-1)
+        return INT_MAX;
+    long long int a=A,b=B;
+    int sign = ((A < 0)^(B < 0))?-1:1;
+    a=abs(a);b=abs(b);
+    int q=0;
+    while(a>=b)
+    { 
+        a=a-b;
+        q++;
+    }
+    return q*sign;
+}
